84c9dba8650ffa5266740ec839014c63103fa64f
Sathish <sathishsekar.ss4@gmail.com>
2024-11-02 22:17:31 +0530

    Trees - Data structures

--- Trees/Tree.java
+++ Trees/Tree.java
@@ -12,12 +12,24 @@ public class Tree{
         }
     }
 
+    public static void printTree(Node node){
+        System.out.println(node.data);
+        if(node.left!=null){
+            printTree(node.left);
+        }
+        if(node.right!=null){
+            printTree(node.right);
+        }
+        return;
+    }
+
     public static void main(String[] args){
         //inser the node and link with each other
         Node root=new Node(1);
         root.left=new Node(2);
         root.right=new Node(3);
-        
+        root.right.right=new Node(4);
+        printTree(root);
     }
 
 }
