b24edcff1f7d2a707d3a85c6e0ecac1722c018ff
Sathish <sathishsekar.ss4@gmail.com>
2024-11-05 22:56:10 +0530

    Sorting - Data structures

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class SelectionSort {
+
+
+    public static void main(String[] args){
+        int arr[]=new int[]{3,2,5,6,8,10,14};
+
+        //basic sorting logic
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[i]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+    
+}
